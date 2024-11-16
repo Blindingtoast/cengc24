6c08899bf4392dfbdbca05c1e885c27e8f5230aa
btran <briantran@cmail.carleton.ca>
2024-11-16 15:57:57 -0500

    Backend

--- backend/src/app/app.py
+++ backend/src/app/app.py
@@ -1,34 +1,30 @@
-from flask import Flask, jsonify, request
-from .models import db, Item
-from sqlalchemy.exc import SQLAlchemyError
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/mydatabase'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+CORS(app)  # Enable CORS to allow React to communicate with Flask
 
-CORS(app)
+# Define upload folder
+UPLOAD_FOLDER = './uploaded_images'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if not request.files:
+        return jsonify({'error': 'No files provided'}), 400
+    
+    files = request.files
+    saved_files = []
 
-@app.route('/api/items', methods=['GET', 'POST'])
-def items():
-    if request.method == 'GET':
-        items = Item.query.all()
-        return jsonify([item.to_dict() for item in items])
-    elif request.method == 'POST':
-        data = request.json
-        new_item = Item(name=data['name'])
-        db.session.add(new_item)
-        try:
-            db.session.commit()
-            return jsonify(new_item.to_dict()), 201
-        except SQLAlchemyError:
-            db.session.rollback()
-            return jsonify({'error': 'Could not save item'}), 500
+    for position, file in files.items():
+        if file:
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            saved_files.append(filename)
 
+    return jsonify({'message': 'Files successfully uploaded', 'files': saved_files}), 200
 
-def main():
-    with app.app_context():
-        db.create_all()
-    app.run(host='0.0.0.0', port=8000)
+def app_main():
+    app.run(debug=True)
